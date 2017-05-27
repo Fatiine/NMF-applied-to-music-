63bcd02161aa5f13c8639d7ea0806febf3b6dc34
rengotj <juliette.rengot@eleves.enpc.fr>
2017-05-27 21:29:31 +0200

    conversion avec les TFCT

--- conversion.m
+++ conversion.m
@@ -1,7 +1,8 @@
 function X = conversion(name)
-    % reads data from the file (.wav !)
-    % returns sampled data, X and plays the sound
-    % Fs is a sample rate for that data
-    [X,Fs] = audioread(name);
-    sound(X,Fs);
+    %convertit le fichier audio en matrice à l'aide de la TFCT
+
+    [X,Fs] = audioread(name); % X = données | Fs = taux d'échantilonnage
+    sound(X,Fs); %jouer l'audio
+    [TFR,T,F]=tfrstft(X); % TFCT
+    X = abs(TFR);
 end

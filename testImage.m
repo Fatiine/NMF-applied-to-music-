81e15e8374c8aab12025ae334177ed330b87fa11
rengotj <juliette.rengot@eleves.enpc.fr>
2017-05-28 21:58:22 +0200

    amélioration test image

--- testImage.m
+++ testImage.m
@@ -1,15 +1,30 @@
-I = imread("lena.jpg")
-Y=I(:,:,1);
-Cb=I(:,:,2);
-Cr=I(:,:,3);
+I = imread('données/lena.jpg');
+%convertir en nuance de gris
+I=get_luminance(I);
+I = double(I);
+%Afficher l'image de départ
+imagesc(I);
+title('Image de départ');
+colormap(gray);
 
+% Décomposition
+[W,H] = NMFeucli(I,100,0.1,10000,1);
+score(I,W*H);
 
+%Afficher les images résultats
+figure(1)
+imagesc(W);
+title('W');
+colormap(gray);
 
+figure(2)
+imagesc(H);
+title('H');
+colormap(gray);
 
-[W,H] = NMFeucli(Y,2,0.1,50,0);
-score(Y,W*H);
-
-
-
+figure(3)
+imagesc(W*H);
+title('W*H')
+colormap(gray);
 
 

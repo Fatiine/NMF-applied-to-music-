81953558b00372ac5deb00e0162ce296501ec1c3
rengotj <juliette.rengot@eleves.enpc.fr>
2017-05-28 15:07:12 +0200

    Test musique qui marche bien

--- conversion.m
+++ conversion.m
@@ -2,7 +2,22 @@ function X = conversion(name)
     %convertit le fichier audio en matrice à l'aide de la TFCT
 
     [X,Fs] = audioread(name); % X = données | Fs = taux d'échantilonnage
-    sound(X,Fs); %jouer l'audio
+    
+    %Visualisations
+    figure(1);
+    plot(X);
+    title('Visualisation des données audio de départ');
+    xlabel('temps');
+    ylabel('amplitude');
+    
+    figure(2);
+    spectrogram(X);
+    title('Spectrogramme des données audio de départ');
+    
+    %jouer l'audio
+    sound(X,Fs);
+    
+    X = X(1:100:end,1:100:end); %echantillonnage
     [TFR,T,F]=tfrstft(X); % TFCT
     X = abs(TFR);
 end
--- testMusique.m
+++ testMusique.m
@@ -1,2 +1,29 @@
 %Etape 1 : acquérir la matrice à partir d'un fichier audio
-X = conversion('Piano.wav');
+X = conversion('données\audio.wav');
+
+[W,H] = NMFeucli(X,2,0.1,50,0);
+score(X,W*H);
+
+figure()
+plot(H(1,:))
+title('Première ligne de H')
+xlabel('temps')
+ylabel('H(1,:)')
+
+figure()
+plot(H(2,:))
+title('Deuxième ligne de H')
+xlabel('temps')
+ylabel('H(2,:)')
+
+figure()
+plot(W(:,1))
+title('Première colonne de W')
+xlabel('fréquence')
+ylabel('W(:,1)')
+
+figure()
+plot(W(:,2))
+title('Deuxième colonne de W')
+xlabel('fréquence')
+ylabel('W(:,2)')
